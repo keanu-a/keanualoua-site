f35598b3811fe84666fe4c8a812fc4605a38310d
keanu-a <keanucaloua@gmail.com>
2025-04-19 23:48:09 -0700

    Initial about page

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React from 'react';
+
+const workExperience = [
+  {
+    title: 'Software Engineer Intern',
+    company: '',
+    year: 0,
+  },
+  {
+    title: 'Web Developer',
+    company: '',
+    year: 0,
+  },
+  {
+    title: 'Project Manager Intern',
+    company: '',
+    year: 0,
+  },
+  {
+    title: 'Undergraduate Teaching Assistant',
+    company: '',
+    year: 0,
+  },
+  {
+    title: 'Private Tutor',
+    company: '',
+    year: 0,
+  },
+];
 
 export default function About() {
   return (
     <div className="font-raleway w-full px-5 max-w-[600px] mx-auto flex flex-col gap-24">
-      <h1 className="text-5xl font-bold md:text-7xl">ABOUT ME</h1>
-      <h1>LIVING AT THE INTERSECTION BETWEEN CREATIVITY ANd SOFTWARE</h1>
+      <h1 className="text-5xl md:text-7xl">ABOUT ME</h1>
+      <p>My focus is on fullstack engineering.</p>
+
+      <div>
+        <h3 className="text-2xl">Work Experience</h3>
+        <ul>
+          {workExperience.map((work, idx) => (
+            <li key={idx}>
+              <p>{idx + 1}</p>
+              <h4>{work.title}</h4>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
